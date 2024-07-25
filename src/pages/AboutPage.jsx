8516c0cfa0dcfcdf2b6a0e41b0f900711016ed54
benjamin-parks <benparks93@gmail.com>
2024-07-25 16:20:02 -0500

    Changed about me page to be in the first person instead of talking about myself in third person.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,10 +10,11 @@ export default function About() {
         <div style={{display:"flex"}}>
             <div style={{width:"50%"}}>
                 <p>
-                    Ben is a full-stack web developer with a passion for creating beautiful, user-friendly applications. He has experience with HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. He is also a FAA licensed drone pilot that has years of experience as a geospatial analyst in Agricultural Analysis.
+                    I am a full-stack web developer with a passion for creating beautiful, user-friendly applications. I have experience with HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I am also a FAA licensed drone pilot that has years of experience as a geospatial analyst in Agricultural Analysis.
                     <br />
                     <br />
-                    He is also an avid waterfowl hunter and duck call maker. He has a passion for the outdoors and enjoys spending time with his family and friends when he is not typing away inside of VS Code.
+                    <br />
+                    I'm also an avid waterfowl hunter and duck call maker. I have a passion for the outdoors and enjoys spending time with his family and friends when I'm not typing away in of VS Code.
                 </p>
             </div>
             <div>
