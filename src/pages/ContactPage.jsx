acecf0fcebdbae08ec1179347a73b981cedeb05c
benjamin-parks <benparks93@gmail.com>
2024-07-25 16:16:20 -0500

    Additional information added to the home page. Also changed up the contact info page to have mailto and tel hrefs.

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -6,11 +6,11 @@ export default function Contact() {
         </div>
         <div>
             <ul style={{listStyle: "none", }}>
-                <li>Email: benparks93@gmail.com</li>
-                <li>Phone: 952-239-7104</li>
-                <li><a href="https://github.com/benjamin-parks">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/ben-parks-a8b04b214/">LinkedIn</a></li>
-                <li><a href="https://x.com/BunMunKun">Twitter</a></li>
+                <li>Email: <a href="mailto:benparks93@gmail.com">benparks93@gmail.com</a></li>
+                <li>Phone: <a href="tel:9522397104">952-239-7104</a></li>
+                <li>Github: <a href="https://github.com/benjamin-parks">Click Here</a></li>
+                <li>LinkedIn: <a href="https://www.linkedin.com/in/ben-parks-a8b04b214/">Click Here</a></li>
+                <li>Twitter: <a href="https://x.com/BunMunKun">Click Here</a></li>
             </ul>
         </div>
     </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,14 @@ import Projects from "../components/Projects";
 
 export default function Home() {
     return (
+    <>
+        <div>
+            <h1>Welcome!</h1>
+            <p>My name is Ben, and I'm a web development student! Below are some of the projects that I've worked on within the last 6 months (newest first) that I am proud enough to share.</p>
+        </div>
         <div>
             <Projects/>
         </div>
+    </>
     );
 }
